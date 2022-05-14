7f6b40eb1c9b9a72d7d09bfd99f1864d31e1698e
gesora96 <nahidahmed9933@gmail.com>
2022-05-14 17:50:54 +0600

    all page header added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import Link from 'next/link';
+import '../styles/globals.css';
+import navStyle from '../styles/nav.module.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav className={navStyle.nav}>
+        <ul>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <Link href='/about'>About</Link>
+          </li>
+          <li>
+            <Link href='/blogs'>Blogs</Link>
+          </li>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
